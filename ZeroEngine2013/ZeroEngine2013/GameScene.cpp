42471cb7986bc8d3b71355539792dbfb4923d6b5
nanen123 <lkjhgf13579@naver.com>
2019-01-18 02:30:35 +0900

    .

--- ZeroEngine2013/ZeroEngine2013/GameScene.cpp
+++ ZeroEngine2013/ZeroEngine2013/GameScene.cpp
@@ -5,42 +5,25 @@
 GameScene::GameScene()
 {
 	ZeroCameraMgr->SetCameraOn();
-	ZeroCameraMgr->SetSpeed(0);
-	ZeroCameraMgr->SetPos(0, 0);
-	Player = new ZeroSprite("Resources/img/Circle.png");
-	BackGround = new ZeroSprite("Resources/BackGround/BackGround.png");
-	isRender = true;
+	ZeroCameraMgr->SetScreen(1280, 720);
+	testBullet = new Bullet();
 }
 
 GameScene::~GameScene()
 {
-	isRender = false;
 }
 
 void GameScene::Update(float eTime)
 {
 	ZeroIScene::Update(eTime);
-	if (ZeroInputMgr->GetKey(VK_RIGHT) == INPUTMGR_KEYON){
-		ZeroCameraMgr->SetPos(ZeroCameraMgr->Pos().x + 200 * eTime, ZeroCameraMgr->Pos().y);
-	}
-	if (ZeroInputMgr->GetKey(VK_LEFT) == INPUTMGR_KEYON){
-		ZeroCameraMgr->SetPos(ZeroCameraMgr->Pos().x - 200 * eTime, ZeroCameraMgr->Pos().y);
-	}
-	if (ZeroInputMgr->GetKey(VK_UP) == INPUTMGR_KEYON){
-		ZeroCameraMgr->SetPos(ZeroCameraMgr->Pos().x, ZeroCameraMgr->Pos().y - 200 * eTime);
-	}
-	if (ZeroInputMgr->GetKey(VK_DOWN) == INPUTMGR_KEYON){
-		ZeroCameraMgr->SetPos(ZeroCameraMgr->Pos().x, ZeroCameraMgr->Pos().y + 200 * eTime);
-	}
-	if (ZeroInputMgr->GetKey('A') == INPUTMGR_KEYDOWN){
-		isRender = !isRender;
-	}
+	testBullet->Update(eTime);
+	player->Update(eTime);
+	
 }
+
 void GameScene::Render()
 {
 	ZeroIScene::Render();
-	BackGround->Render();
-	if (isRender){
-		Player->Render();
-	}
+	testBullet->Render();
+	player->Render();
 }
--- ZeroEngine2013/ZeroEngine2013/GameScene.h
+++ ZeroEngine2013/ZeroEngine2013/GameScene.h
@@ -3,15 +3,19 @@
 #include "ZeroSprite.h"
 #include "ZeroInputManager.h"
 #include "ZeroCameraManager.h"
+#include <list>
+#include "Player.h"
+#include "Bullet.h"
+
 class GameScene :
 	public ZeroIScene
 {
 public:
 	bool isRender;
+	Bullet *testBullet;
 	GameScene();
 	~GameScene();
 	void Update(float eTime);
 	void Render();
-	ZeroSprite * Player;
-	ZeroSprite * BackGround;
+	
 };
