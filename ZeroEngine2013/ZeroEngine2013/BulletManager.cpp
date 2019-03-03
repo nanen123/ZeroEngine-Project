713b6fd9e318690f995aec7230295a69a8d589b9
nanen123 <lkjhgf13579@naver.com>
2019-03-04 05:14:06 +0900

    modifying value of angle

--- ZeroEngine2013/ZeroEngine2013/BulletManager.cpp
+++ ZeroEngine2013/ZeroEngine2013/BulletManager.cpp
@@ -38,15 +38,14 @@ void BulletManager::SpawnBullet()
 	float angle;
 	if (Time >= 1)
 	{
-		Bullet* newB = new Bullet();
-		newB->p = p;
+		Bullet* newB = new Bullet(p);
 		while (true)
 		{
 			newB->Pos().x = rand() % 1280; // 640
 			newB->Pos().y = rand() % 720; // 360
 			if (newB->Pos().Distance(Center) > 300) break;
 		}
-		angle = atan2(rand() % 660 + 60 - newB->Pos().y, rand() % 940 + 340 - newB->Pos().x);
+		angle = atan2((rand() % 400 + 254) - newB->Pos().y, (rand() % 500 + 400) - newB->Pos().x);
 		/*if (newB->Pos().x > 1080 / 2)
 		{
 			newB->dirX = -cos(rand() % 90);
@@ -63,5 +62,20 @@ void BulletManager::SpawnBullet()
 		
 		BulletList.push_back(newB);
 		Time -= 1;
+	}
+	
+}
+
+void BulletManager::Destroy()
+{
+	for (auto iter = BulletList.begin(); iter != BulletList.end();)
+	{
+		if ((*iter)->Pos().x > 1280 || (*iter)->Pos().x < 0 ||
+			(*iter)->Pos().y > 720 || (*iter)->Pos().y < 0)
+		{
+			delete *iter;
+			iter = BulletList.erase(iter);
+		}
+		else iter++;
 	}
 }
--- ZeroEngine2013/ZeroEngine2013/BulletManager.h
+++ ZeroEngine2013/ZeroEngine2013/BulletManager.h
@@ -23,4 +23,5 @@ public:
 	void Render();
 
 	void SpawnBullet();
+	void Destroy();
 };
